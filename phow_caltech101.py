c9242e6da5631f575851e55610f7c096000f0328
Ludwig Schmidt-Hackenberg <ludwig@schmidt-hackenberg.net>
2013-04-08 14:01:41 +0300

    Moved configuration parameters further to the top

--- phow_caltech101.py
+++ phow_caltech101.py
@@ -32,6 +32,62 @@ VERBOSE = True  # set to 'SVM' if you want to get the svm output
 MULTIPROCESSING = False
 
 
+class Configuration(object):
+    def __init__(self, identifier=''):
+        self.calDir = '../../../datasets/Caltech/101_ObjectCategories'
+        self.dataDir = 'tempresults'  # should be resultDir or so
+        if not exists(self.dataDir):
+            makedirs(self.dataDir)
+            print "folder " + self.dataDir + " created"
+        self.autoDownloadData = True
+        self.numTrain = 15
+        self.numTest = 15
+        self.imagesperclass = self.numTrain + self.numTest
+        self.numClasses = 102
+        self.numWords = 600
+        self.numSpatialX = [2, 4]
+        self.numSpatialY = [2, 4]
+        self.quantizer = 'vq'  # kdtree from the .m version not implemented
+        self.svm = SVMParameters(C=10, solver='pegasos', biasMultiplier=1)
+        self.phowOpts = PHOWOptions(Verbose=False, Sizes=[4, 6, 8, 10], Step=3)
+        self.clobber = False
+        self.tinyProblem = TINYPROBLEM
+        self.prefix = 'baseline'
+        self.randSeed = 1
+        self.verbose = True
+        self.extensions = [".jpg", ".bmp", ".png", ".pgm", ".tif", ".tiff"]
+        self.images_for_histogram = 30
+        self.numbers_of_features_for_histogram = 100000
+        
+        self.vocabPath = join(self.dataDir, identifier + '-vocab.py.mat')
+        self.histPath = join(self.dataDir, identifier + '-hists.py.mat')
+        self.modelPath = join(self.dataDir, self.prefix + identifier + '-model.py.mat')
+        self.resultPath = join(self.dataDir, self.prefix + identifier + '-result')
+        
+        if self.tinyProblem:
+            print "Using 'tiny' protocol with different parameters than the .m code"
+            self.prefix = 'tiny'
+            self.numClasses = 5
+            self.images_for_histogram = 10
+            self.numbers_of_features_for_histogram = 1000
+            self.numTrain
+            self.numSpatialX = 2
+            self.numWords = 100
+            self.numTrain = 2
+            self.numTest = 2
+            self.phowOpts = PHOWOptions(Verbose=2, Sizes=7, Step=5)
+
+        # tests and conversions
+        self.phowOpts.Sizes = ensure_type_array(self.phowOpts.Sizes)
+        self.numSpatialX = ensure_type_array(self.numSpatialX)
+        self.numSpatialY = ensure_type_array(self.numSpatialY)
+        if (self.numSpatialX != self.numSpatialY).any():
+            messageformat = [str(self.numSpatialX), str(self.numSpatialY)]
+            import pdb; pdb.set_trace()
+            message = "(self.numSpatialX != self.numSpatialY), because {0} != {1}".format(*messageformat)
+            raise ValueError(message)
+
+
 def ensure_type_array(data):
     if (type(data) is not ndarray):
         if (type(data) is list):
@@ -124,62 +180,6 @@ class PHOWOptions(object):
         self.Verbose = Verbose
         self.Sizes = Sizes
         self.Step = Step
-
-
-class Configuration(object):
-    def __init__(self, identifier=''):
-        self.calDir = '../../../datasets/Caltech/101_ObjectCategories'
-        self.dataDir = 'tempresults'  # should be resultDir or so
-        if not exists(self.dataDir):
-            makedirs(self.dataDir)
-            print "folder " + self.dataDir + " created"
-        self.autoDownloadData = True
-        self.numTrain = 15
-        self.numTest = 15
-        self.imagesperclass = self.numTrain + self.numTest
-        self.numClasses = 102
-        self.numWords = 600
-        self.numSpatialX = [2, 4]
-        self.numSpatialY = [2, 4]
-        self.quantizer = 'vq'  # kdtree from the .m version not implemented
-        self.svm = SVMParameters(C=10, solver='pegasos', biasMultiplier=1)
-        self.phowOpts = PHOWOptions(Verbose=False, Sizes=[4, 6, 8, 10], Step=3)
-        self.clobber = False
-        self.tinyProblem = TINYPROBLEM
-        self.prefix = 'baseline'
-        self.randSeed = 1
-        self.verbose = True
-        self.extensions = [".jpg", ".bmp", ".png", ".pgm", ".tif", ".tiff"]
-        self.images_for_histogram = 30
-        self.numbers_of_features_for_histogram = 100000
-        
-        self.vocabPath = join(self.dataDir, identifier + '-vocab.py.mat')
-        self.histPath = join(self.dataDir, identifier + '-hists.py.mat')
-        self.modelPath = join(self.dataDir, self.prefix + identifier + '-model.py.mat')
-        self.resultPath = join(self.dataDir, self.prefix + identifier + '-result')
-        
-        if self.tinyProblem:
-            print "Using 'tiny' protocol with different parameters than the .m code"
-            self.prefix = 'tiny'
-            self.numClasses = 5
-            self.images_for_histogram = 10
-            self.numbers_of_features_for_histogram = 1000
-            self.numTrain
-            self.numSpatialX = 2
-            self.numWords = 100
-            self.numTrain = 2
-            self.numTest = 2
-            self.phowOpts = PHOWOptions(Verbose=2, Sizes=7, Step=5)
-
-        # tests and conversions
-        self.phowOpts.Sizes = ensure_type_array(self.phowOpts.Sizes)
-        self.numSpatialX = ensure_type_array(self.numSpatialX)
-        self.numSpatialY = ensure_type_array(self.numSpatialY)
-        if (self.numSpatialX != self.numSpatialY).any():
-            messageformat = [str(self.numSpatialX), str(self.numSpatialY)]
-            import pdb; pdb.set_trace()
-            message = "(self.numSpatialX != self.numSpatialY), because {0} != {1}".format(*messageformat)
-            raise ValueError(message)
 
 
 def get_classes(datasetpath, numClasses):
